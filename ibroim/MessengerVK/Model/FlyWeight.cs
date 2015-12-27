de9d8bab921e57b7dcdc931450787d24117c9076
ibroimAvazov <ibroim_avazov@hotmail.com>
2015-12-27 16:15:18 +0300

    Vkmessenger

---
+++ ibroim/MessengerVK/Model/FlyWeight.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Image = System.Windows.Controls.Image;
+
+namespace MessengerVK
+{
+    public class FlyWeight
+    {
+        public abstract class Smile
+        {
+            public Bitmap smile { get; set; }
+            public abstract void Build(string name);
+        }
+
+        public class SmileFactory
+        {
+            IDictionary<string, Bitmap> smiles = new Dictionary<string, Bitmap>();
+
+            public SmileFactory()
+            {
+                smiles.Add(":[)]", MessengerVK.Properties.Resources._01);
+                smiles.Add("(:[(])", MessengerVK.Properties.Resources._02);
+                smiles.Add("😊", MessengerVK.Properties.Resources._03);
+                smiles.Add("😃", MessengerVK.Properties.Resources._04);
+                smiles.Add(":-D", MessengerVK.Properties.Resources._05);
+                smiles.Add("😆", MessengerVK.Properties.Resources._06);
+                smiles.Add("😜", MessengerVK.Properties.Resources._07);
+                smiles.Add("😋", MessengerVK.Properties.Resources._08);
+                smiles.Add("😍", MessengerVK.Properties.Resources._09);
+                smiles.Add("😎", MessengerVK.Properties.Resources._10);
+                smiles.Add("😒", MessengerVK.Properties.Resources._11);
+                smiles.Add("😏", MessengerVK.Properties.Resources._12);
+            }
+
+            public Bitmap GetSmile(string key)
+            {
+                if (smiles.ContainsKey(key))
+                    return smiles[key];
+                else
+                    return null;
+            }
+        }
+    }
+}
---
+++ ibroim/MessengerVK/Model/Interpreter/InterpreterMessage.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text.RegularExpressions;
+using MessengerVK.Builder;
+using VkNet.Enums;
+
+namespace MessengerVK.Interpreter
+{
+    /// <summary>
+    /// The 'AbstractExpression' abstract class
+    /// </summary>
+    public abstract class AbstractExpression
+    {
+        public abstract string Interpret(Message message, int IndexSelectedFriend, String SelectedFriendName, string SelectedFriendLastName);
+
+        public static void HTMLTableCreater(out string path1, Message message, out string path2, string SelectedFriendName, string SelectedFriendLastName)
+        {
+            path1 = "<table align=" + "center" + "><tbody><tr><th width=" + "50px" + "><div><a><font color=" +
+                    "#002133" + ">" + SelectedFriendName + "<br/>" +
+                    SelectedFriendLastName +
+                    "</font></div></th><th width=" + "400px" + "><div><a align=" + "left><font color=" +
+                    "#001433>" + message.Body + "</font><a/></div></div></th><th width=" + "50px" + "><div><a><font color=" +
+                    "#bfbfbf>" + message.DateTime +
+                    "</font></a></div></th></tr></tbody></table>";
+            path2 = "<table align=" + "center" + "><tbody><tr><th width=" + "50px" + "><div><a><font color=" +
+                    "#002133" + ">" + Admin.GetInstance().UserSingelton.FirstName + "<br/>" +
+                    Admin.GetInstance().UserSingelton.LastName +
+                    "</font></div></th><th width=" + "400px" + "><div><a align=" + "left><font color=" +
+                    "#001433>" + message.Body + "</font><a/></div></div></th><th width=" + "50px" + "><div><a><font color=" +
+                    "#bfbfbf>" + message.DateTime +
+                    "</font></a></div></th></tr></tbody></table>";
+        }
+
+
+    }
+    /// <summary>
+    /// The 'TerminalExpression' class for message whichtr no emoji
+    /// </summary>
+    public class TerminalExpression : AbstractExpression
+    {
+        public override string Interpret(Message message, int IndexSelectedFriend, string SelectedFriendName,
+            string SelectedFriendLastName)
+        {
+           
+        string[]
+            patterns = new[]
+            {":[)]", "(:[(])", "😊", "😃", ":-D", "😆", "😜", "😋", "😍", "😎", "😒", "😏"};
+            string beginImgTag = "<img src=data:image/png;base64,";
+            string endImgTag = "></img>";
+            Bitmap bitmap = new Bitmap(MessengerVK.Properties.Resources._01);
+            FlyWeight.SmileFactory smileFactory = new FlyWeight.SmileFactory();
+            smileFactory.GetSmile(":-[)]");
+            for (int i = 0; i < patterns.Length; i++)
+            {
+                message.Body = Regex.Replace(message.Body, patterns[i],
+                    replacement: beginImgTag + ImageToBase64(smileFactory.GetSmile(patterns[i]), ImageFormat.Png) + endImgTag);
+            }
+            string HtmlTableWithMessage = string.Empty, path1, path2;
+            HTMLTableCreater(out path1, message, out path2, SelectedFriendName, SelectedFriendLastName);
+            HtmlTableWithMessage = message.TypeMessage == MessageType.Received ? path1 : path2;
+            return HtmlTableWithMessage;
+
+        }
+        public string ImageToBase64(System.Drawing.Bitmap image, System.Drawing.Imaging.ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Convert Image to byte[]
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+
+                // Convert byte[] to Base64 String
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
+        }
+
+    }
+    /// <summary>
+    /// The 'NonterminalExpression' class for message with emoji
+    /// </summary>
+    public class NonTerminalExpression : AbstractExpression
+    {
+        public override string Interpret(Message message, int IndexSelectedFriend, string SelectedFriendName, string SelectedFriendLastName)
+        {
+
+            string HtmlTableWithMessage = string.Empty, path1, path2;
+            HTMLTableCreater(out path1, message, out path2, SelectedFriendName, SelectedFriendLastName);
+            HtmlTableWithMessage = message.TypeMessage == MessageType.Received ? path1 : path2;
+            return HtmlTableWithMessage;
+        }
+    }
+}
